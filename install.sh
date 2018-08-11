90f2dc11bc9c2a4d8243e4ae1db60eacd9640cbd
Trevor Vallender <tsv@tsv.me.uk>
2018-08-11 09:47:41 +0100

    Added install script for Linux

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+ln ./.emacs ~/.emacs
+ln ./.gitconfig ~/.gitconfig
+ln ./.vimrc ~/.vimrc
+ln ./.profile ~/.profile
+ln ./.tmux.conf ~/.tmux.conf
+ln -s ./.tmux.d ~/.tmux.d
+ln ./.zshrc ~/.zshrc
+ln ./.zprofile ~/.zprofile
